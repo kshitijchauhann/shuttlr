6c5561dad30442e46ca5d7bca34e1ae87ea0d8a3
Kshitij Chauhan <chauhankay18@gmail.com>
2025-07-18 18:05:42 +0530

    removed hardcoded port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -81,4 +81,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
 
-server.listen(3000, () => console.log("Express + websocket server running on port 3000!"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Express + WebSocket server running on port ${PORT}!`);
+});

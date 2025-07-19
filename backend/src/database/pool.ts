dfc2e82439f816277b37455df837b813cd22e412
Kshitij Chauhan <chauhankay18@gmail.com>
2025-07-19 19:16:01 +0530

    db connection url

--- backend/src/database/pool.ts
+++ backend/src/database/pool.ts
@@ -6,12 +6,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  ssl: process.env.DB_SSL === "true", 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // required for Neon
+  }
 });
 
 export default pool;
